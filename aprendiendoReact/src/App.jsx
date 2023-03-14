580ef0281ff107758bb04817f502f081effb8ae6
Publico <julioarenas85@gmail.com>
2023-03-14 16:12:34 -0500

    Comprender Onclick y OnChange

--- aprendiendoReact/src/App.jsx
+++ aprendiendoReact/src/App.jsx
@@ -4,19 +4,28 @@ import './App.css'
 
 function App() {
   const [name, setName] = useState("julio");
-
+  const [lastName, setApellido]= useState("Arenas");
   const actulizar = (e)=> {
     const valor = e.target.value;
     setName(valor);
     console.log(name);
   }
+  const actualizarApellido= (e)=>{
+    const apellido= e.target.value;
+    setApellido(apellido);
+  }
   
+  const guardarClick = ()=>{
+    console.log(name);
+  }
 
   return (
     <div className="App">
       <h1>Formulario</h1>
       <label for="nombre">Nombre:</label>
       <input type="text" id="nombre" name="nombre" value={name} onChange={actulizar}></input>
+      <input type="text" id="apellido" name="apellido" value={lastName} onChange={actualizarApellido}></input>
+      <button onClick={guardarClick}>Guardar</button>
 
     </div>
   )
